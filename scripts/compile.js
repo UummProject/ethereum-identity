3279fee2ea7b8344bf36da6a57caee9659ac0365
Xavi Vives <xavings@gmail.com>
2018-03-11 21:16:15 +0100

    Creating script to compile Identity contarcts with imports without using truffle

---
+++ scripts/compile.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const solc = require('solc')
+const mkdirp = require ('mkdirp')
+
+var input = {
+    'ERC725.sol': fs.readFileSync('contracts/ERC725.sol', 'utf8'),
+    'ERC735.sol': fs.readFileSync('contracts/ERC735.sol', 'utf8'),
+    'Identity.sol': fs.readFileSync('contracts/Identity.sol', 'utf8'),
+};
+let compiledContract = solc.compile({sources: input}, 1);
+let abi = compiledContract.contracts['Identity.sol:Identity'].interface;
+
+let bytecode = '0x'+compiledContract.contracts['Identity.sol:Identity'].bytecode;
+
+console.log("Contract compiled")
+
+fs.writeFile("build/Identity.abi.json", abi, function(err)
+{
+    if(err)
+        return console.log(err);
+    else
+        console.log("Abi file created at build/Identity.abi.json");
+}); 
+
+fs.writeFile("build/Identity.bin",bytecode, function(err)
+{
+    if(err)
+        return console.log(err);
+    else
+        console.log("Bytecode file created at build/Identity.bin");
+}); 
