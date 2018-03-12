4fda7246f98f70bbf5557f1c79797e4a3916b019
Xavi Vives <xavings@gmail.com>
2018-03-12 13:46:51 +0100

    Deploying identity contract

--- scripts/basicScenario.js
+++ scripts/basicScenario.js
@@ -1,23 +1,20 @@
 
 const Web3 = require ('web3')
-//const Contract = require ('truffle-contract')
-//const IdentityArtifact = require('../build/contracts/Identity.json')
+
 let IdentityAbi = require('../build/Identity.json')
 let IdentityBin = require('../build/Identity.bin.json')
-
-//console.log(JSON.parse(IdentityAbi))
 
 Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send
 let wsProvider = new Web3.providers.WebsocketProvider("ws://localhost:8545")
 let web3 = new Web3(wsProvider)
 
-//let IdentityContract = Contract(IdentityArtifact)
 let BaseIdentityContract = new web3.eth.Contract(IdentityAbi)
 BaseIdentityContract.setProvider(wsProvider)
 
 let accounts = []
 let userIdentityContract = {}
-
+let deployGasCost = 0
+let deployTransaction = {}
 
 run =()=>{
     return new Promise((resolve, reject)=>{
@@ -27,24 +24,24 @@ run =()=>{
             accounts = a
         })
         .then(()=>{
-            let deployTransaction = BaseIdentityContract.deploy({ data:IdentityBin, arguments:[]})
+            deployTransaction = BaseIdentityContract.deploy({ data:IdentityBin, arguments:[]})
+            return deployTransaction.estimateGas()
         })
-
-        .then(()=>{
-            
-            resolve(1)
+        .then((gasCost)=>{
+            deployGasCost = gasCost
+            deployTransaction.send( {from: accounts[0], gas: deployGasCost, gasPrice: 1})
+            .on('error', function(error){ console.error(error) })
+            .on('transactionHash', function(transactionHash){ console.log(transactionHash)})
+            .on('confirmation', function(confirmationNumber, receipt){ console.log (confirmationNumber, receipt) })
+            .on('receipt', function(receipt){ console.log(receipt.contractAddress)})
+            .then(function(newContractInstance){
+                console.log(newContractInstance.options.address) // instance with the new contract address
+                resolve(1)
+            });
         })
     })
-}
-
+};
 
 run()
-.then(() =>  {
-    console.log("Done!");
-    process.exit(0);
-})
-.catch((error)=>
-{
-    console.log(error)
-    process.exit(1)
-})
+.then(() =>  {console.log("Done!"); process.exit(0);})
+.catch((error)=>{console.log(error);process.exit(1);})
