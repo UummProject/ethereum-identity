2eb5b990aad5f00b3c146e0222bc8d924302d3fc
Xavi Vives <xavings@gmail.com>
2018-03-20 15:33:56 +0100

    Merged latest implementation

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -6,210 +6,291 @@
 
     contract Identity is ERC725, ERC735 {
 
-        mapping (address => uint256) keys;
-        mapping (bytes32 => Claim) claims;
-        mapping (uint256 => address[]) keysByType;
-        mapping (uint256 => bytes32[]) claimsByType;
-        mapping (bytes32 => uint256) indexes;
-        mapping (bytes32 => Transaction) txx;
+        //ERC725
+        uint256 executionNonce;
 
-        uint nonce = 0;
-
-        struct Transaction {
+        struct Execution {
             address to;
-            uint value;
+            uint256 value;
             bytes data;
-            uint nonce;
+            bool approved;
+            bool executed;
         }
 
+        mapping (bytes32 => Key) keys;
+        mapping (uint256 => bytes32[]) keysByPurpose;
+        mapping (uint256 => Execution) executions;
 
-        modifier managerOnly {
-            require(keys[msg.sender] == MANAGEMENT_KEY);
-            _;
+        event ExecutionFailed(uint256 indexed executionId, address indexed to, uint256 indexed value, bytes data);
+
+        function KeyHolder() public {
+            bytes32 _key = keccak256(msg.sender);
+            keys[_key].key = _key;
+            keys[_key].purpose = 1;
+            keys[_key].keyType = 1;
+            keysByPurpose[1].push(_key);
+            emit KeyAdded(_key, keys[_key].purpose, 1);
         }
 
-        modifier managerOrSelf {
-            require(keys[msg.sender] == MANAGEMENT_KEY || msg.sender == address(this));
-            _;
+        function getKey(bytes32 _key)
+            public
+            view
+            returns(uint256 purpose, uint256 keyType, bytes32 key)
+        {
+            return (keys[_key].purpose, keys[_key].keyType, keys[_key].key);
         }
 
-        modifier actorOnly {
-            require(keys[msg.sender] == ACTION_KEY);
-            _;
+        function getKeyPurpose(bytes32 _key)
+            public
+            view
+            returns(uint256 purpose)
+        {
+            return (keys[_key].purpose);
         }
 
-        modifier claimSignerOnly {
-            require(keys[msg.sender] == CLAIM_SIGNER_KEY);
-            _;
+        function getKeysByPurpose(uint256 _purpose)
+            public
+            view
+            returns(bytes32[] _keys)
+        {
+            return keysByPurpose[_purpose];
         }
 
+        function addKey(bytes32 _key, uint256 _purpose, uint256 _type)
+            public
+            returns (bool success)
+        {
+            require(keys[_key].key != _key); // Key should not already exist
+            if (msg.sender != address(this)) {
+            require(keyHasPurpose(keccak256(msg.sender), 1)); // Sender has MANAGEMENT_KEY
+            }
 
-        function Identity() public {
-            _addKey(msg.sender, MANAGEMENT_KEY);
-        }
+            keys[_key].key = _key;
+            keys[_key].purpose = _purpose;
+            keys[_key].keyType = _type;
 
+            keysByPurpose[_purpose].push(_key);
 
-        function addKey(address _key, uint256 _type) public managerOrSelf returns (bool success) {
-            _addKey(_key, _type);
-        }
+            emit KeyAdded(_key, _purpose, _type);
 
-
-        function removeKey(address _key) public managerOrSelf returns (bool success) {
-            _removeKey(_key);
-        }
-
-
-        function replaceKey(address _oldKey, address _newKey) public managerOrSelf returns (bool success) {
-            _addKey(_newKey, getKeyType(_oldKey));
-            _removeKey(_oldKey);
             return true;
         }
 
-
-
-        function execute(
-            address _to,
-            uint256 _value,
-            bytes _data
-        ) 
-            public 
-            returns (bytes32 executionId) 
-        {
-            uint256 senderKey = keys[msg.sender];
-            require(senderKey == MANAGEMENT_KEY || senderKey == ACTION_KEY);
-            executionId = keccak256(_to, _value, _data, nonce);
-            ExecutionRequested(executionId, _to, _value, _data);
-            txx[executionId] = Transaction (
-                {
-                    to: _to,
-                    value: _value,
-                    data: _data,
-                    nonce: nonce
-                });
-            if (senderKey == MANAGEMENT_KEY) {
-                approve(executionId, true);
-            }
-        }
-
-        function approve(
-            bytes32 _id,
-            bool _approve
-        ) 
+        function approve(uint256 _id, bool _approve)
             public
-            managerOnly
             returns (bool success)
         {
-            require(txx[_id].nonce == nonce);
-            nonce++;
-            if (_approve) {
-                success = txx[_id].to.call.value(txx[_id].value)(txx[_id].data);
-            } 
+            keyHasPurpose(keccak256(msg.sender),2);
+
+            emit Approved(_id, _approve);
+
+            if (_approve != true) {
+                executions[_id].approved = false;
+                return true;
+            }
+
+            executions[_id].approved = true;
+            success = executions[_id].to.call(executions[_id].data);
+            if (success) {
+                executions[_id].executed = true;
+                emit Executed(
+                    _id,
+                    executions[_id].to,
+                    executions[_id].value,
+                    executions[_id].data
+                );
+                return;
+            } else {
+                emit ExecutionFailed(
+                    _id,
+                    executions[_id].to,
+                    executions[_id].value,
+                    executions[_id].data
+                );
+                return;
+            }
+
+            return true;
         }
 
+        function execute(address _to, uint256 _value, bytes _data)
+            public
+            returns (uint256 executionId)
+        {
+            require(!executions[executionNonce].executed);
+            executions[executionNonce].to = _to;
+            executions[executionNonce].value = _value;
+            executions[executionNonce].data = _data;
+
+            emit ExecutionRequested(executionNonce, _to, _value, _data);
+
+            if (keyHasPurpose(keccak256(msg.sender),1) || keyHasPurpose(keccak256(msg.sender),2)) {
+                approve(executionNonce, true);
+            }
+
+            executionNonce++;
+            return executionNonce-1;
+        }
+
+        function removeKey(bytes32 _key)
+            public
+            returns (bool success)
+        {
+            require(keys[_key].key == _key);
+            emit KeyRemoved(keys[_key].key, keys[_key].purpose, keys[_key].keyType);
+
+            /* uint index;
+            (index,) = keysByPurpose[keys[_key].purpose.indexOf(_key);
+            keysByPurpose[keys[_key].purpose.removeByIndex(index); */
+
+            delete keys[_key];
+
+            return true;
+        }
+
+        function keyHasPurpose(bytes32 _key, uint256 _purpose)
+            public
+            view
+            returns(bool result)
+        {
+            bool isThere;
+            if (keys[_key].key == 0) return false;
+            isThere = keys[_key].purpose <= _purpose;
+            return isThere;
+        }
+
+        //EIP735
+        bytes32 claimId;
+        mapping (bytes32 => Claim) claims;
+        mapping (uint256 => bytes32[]) claimsByType;
 
         function addClaim(
             uint256 _claimType,
+            uint256 _scheme,
             address _issuer,
-            uint256 _signatureType,
             bytes _signature,
-            bytes _claim,
+            bytes32 _data,
             string _uri
-        ) 
-            public 
-            claimSignerOnly 
-            returns (bytes32 claimId) 
+        )
+            public
+            returns (bytes32 claimRequestId)
         {
             claimId = keccak256(_issuer, _claimType);
-            claims[claimId] = Claim(
-                {
-                    claimType: _claimType,
-                    issuer: _issuer,
-                    signatureType: _signatureType,
-                    signature: _signature,
-                    claim: _claim,
-                    uri: _uri
-                }
+            //KeyHolder issuer = KeyHolder(issuer);
+
+            if (msg.sender != address(this)) {
+            require(keyHasPurpose(keccak256(msg.sender), 3));
+            }
+
+            if (claims[claimId].issuer != _issuer) {
+                claimsByType[_claimType].push(claimId);
+            }
+
+            claims[claimId].claimType = _claimType;
+            claims[claimId].scheme = _scheme;
+            claims[claimId].issuer = _issuer;
+            claims[claimId].signature = _signature;
+            claims[claimId].data = _data;
+            claims[claimId].uri = _uri;
+
+            emit ClaimAdded(
+                claimId,
+                _claimType,
+                _scheme,
+                _issuer,
+                _signature,
+                _data,
+                _uri
             );
-            indexes[keccak256(_issuer, _claimType)] = claimsByType[_claimType].length;
-            claimsByType[_claimType].push(claimId);
+
+            return claimId;
         }
 
-
         function removeClaim(bytes32 _claimId) public returns (bool success) {
-            Claim memory c = claims[_claimId];
-            require(msg.sender == c.issuer || keys[msg.sender] == MANAGEMENT_KEY || msg.sender == address(this));
-            uint claimIdTypePos = indexes[_claimId];
-            delete indexes[_claimId];
-            bytes32[] storage claimsTypeArr = claimsByType[c.claimType];
-            bytes32 replacer = claimsTypeArr[claimsTypeArr.length-1];
-            claimsTypeArr[claimIdTypePos] = replacer;
-            indexes[replacer] = claimIdTypePos;
+            require(
+                msg.sender == claims[_claimId].issuer ||
+                msg.sender == address(this)
+            );
+
+            emit ClaimRemoved(
+                _claimId,
+                claims[_claimId].claimType,
+                claims[_claimId].scheme,
+                claims[_claimId].issuer,
+                claims[_claimId].signature,
+                claims[_claimId].data,
+                claims[_claimId].uri
+            );
+
             delete claims[_claimId];
-            claimsTypeArr.length--;
             return true;
         }
 
-
-        function _addKey(address _key, uint256 _type) private {
-            require(keys[_key] == 0);
-            KeyAdded(_key, _type);
-            keys[_key] = _type;
-            indexes[keccak256(_key, _type)] = keysByType[_type].length;
-            keysByType[_type].push(_key);
-        }
-
-
-        function _removeKey(address _key) private {
-            uint256 kType = keys[_key];
-            KeyRemoved(_key, kType);
-            address[] storage keyArr = keysByType[kType];
-            if (msg.sender != address(this) && kType == MANAGEMENT_KEY && keyArr.length == 1) {
-                revert();
-            }
-            bytes32 oldIndex = keccak256(_key, kType);
-            uint index = indexes[oldIndex];
-            delete indexes[oldIndex];
-            address replacer = keyArr[keyArr.length-1];
-            keyArr[index] = replacer;
-            indexes[keccak256(replacer, keys[replacer])] = index;
-            keyArr.length--;
-            delete keys[_key];
-        }
-
-
-        function getKeyType(address _key) public constant returns(uint256 keyType) {
-            return keys[_key];
-        }
-
-
-        function getKeysByType(uint256 _type) public constant returns(address[]) {
-            return keysByType[_type];
-        }
-
-
-        function getClaim(
-            bytes32 _claimId
-        )
-            public 
-            constant 
-            returns
-                (uint256 claimType,
+        function getClaim(bytes32 _claimId)
+            public
+            constant
+            returns(
+                uint256 claimType,
+                uint256 scheme,
                 address issuer,
-                uint256 signatureType,
                 bytes signature,
-                bytes claim,
-                string uri)
+                bytes32 data,
+                string uri
+            )
         {
-            Claim memory _claim = claims[_claimId];
-            return (_claim.claimType, _claim.issuer, _claim.signatureType, _claim.signature, _claim.claim, _claim.uri);
+            return (
+                claims[_claimId].claimType,
+                claims[_claimId].scheme,
+                claims[_claimId].issuer,
+                claims[_claimId].signature,
+                claims[_claimId].data,
+                claims[_claimId].uri
+            );
         }
 
+        function getClaimSig(bytes32 _claimId)
+            public
+            constant
+            returns(
+            bytes32 data,
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+            )
+        {
+            bytes32 ra;
+            bytes32 sa;
+            uint8 va;
 
-        function getClaimsIdByType(uint256 _claimType) public constant returns(bytes32[]) {
+            bytes memory sig = claims[_claimId].signature;
+            bytes32 claimData = claims[_claimId].data;
+
+            // Check the signature length
+            if (sig.length != 65) {
+            return (0, 0, 0, 0);
+            }
+
+            // Divide the signature in r, s and v variables
+            assembly {
+            ra := mload(add(sig, 32))
+            sa := mload(add(sig, 64))
+            va := byte(0, mload(add(sig, 96)))
+            }
+
+            if (va < 27) {
+            va += 27;
+            }
+
+            return (claimData, ra, sa, va);
+        }
+
+        function getClaimIdsByType(uint256 _claimType)
+            public
+            constant
+            returns(bytes32[] claimIds)
+        {
             return claimsByType[_claimType];
         }
-
-
     }
 
 
