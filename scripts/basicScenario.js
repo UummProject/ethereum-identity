8d0efbfa0672389d99cb1b693922cd6519ae9b16
Xavi Vives <xavings@gmail.com>
2018-03-24 13:21:57 +0100

    Adding key functionality. Using hash as claim data

--- scripts/basicScenario.js
+++ scripts/basicScenario.js
@@ -10,11 +10,8 @@ Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.pr
 let wsProvider = new Web3.providers.WebsocketProvider("ws://localhost:8545")
 let web3 = new Web3(wsProvider)
 
-let BaseIdentityContract = new web3.eth.Contract(IdentityAbi)
-BaseIdentityContract.setProvider(wsProvider)
-
-let accounts = []
-let dids = {}
+let accounts = [] //
+let dids = {}// contractOwnerAddress:contractAddress
 
 let EMITTER = 0
 let USER1 = 1
@@ -26,10 +23,8 @@ let CLAIM_CONTENT = '{ "did": "did:entityUserBelongs:userEntityId" }'
 
 createIdentityContractInstance = (contractAddress) =>
 {
-    let identityContract = new web3.eth.Contract(IdentityAbi)
+    let identityContract = new web3.eth.Contract(IdentityAbi, contractAddress)
     identityContract.setProvider(wsProvider)
-    if(contractAddress)
-        identityContract.options.address = address
     return identityContract
 }
 
@@ -55,7 +50,7 @@ newDid =(account)=>
             //.on('confirmation', function(confirmationNumber, receipt){ console.log ("confirmationNumber",account.address,confirmationNumber) })
             //.on('receipt', function(receipt){ console.log("receipt", account.address,receipt.blockNumber)})
             .then(function(newContractInstance){
-                console.log("Did created",account.address, newContractInstance.contractAddress)
+                console.log("Did created", account.address, newContractInstance.contractAddress)
                 dids[account.address] = newContractInstance.contractAddress
                 resolve(newContractInstance)
             });
@@ -80,6 +75,48 @@ createAllDids=(accounts)=>
             .catch(reject)
         }
         createNextDid(0)
+    })
+}
+
+addKey=(account, contractAddress, key, keyPurpose, keyType)=>
+{
+    //Specs:https://github.com/ethereum/EIPs/issues/725
+
+    /*
+    Key purposes:
+    1: MANAGEMENT keys, which can manage the identity
+    2: ACTION keys, which perform actions in this identities name (signing, logins, transactions, etc.)
+    3: CLAIM signer keys, used to sign claims on other identities which need to be revokable.
+    4: ENCRYPTION keys, used to encrypt data e.g. hold in claims.
+    */
+
+    /*KeyType: 
+    1 : ECDSA
+    2 : RSA
+    */
+
+    return new Promise((resolve, reject)=>{
+
+        let identityContract = createIdentityContractInstance(contractAddress)
+        let addKeyTransaction = identityContract.methods.addKey(key, keyPurpose, keyType)
+        console.log(addKeyTransaction)
+        let encodedAbi = addKeyTransaction.encodeABI()
+        let transaction = {
+            gas: deployIdentityGasCost,
+            gasPrice: GAS_PRICE,
+            data: encodedAbi,
+            from: account.address
+        }
+
+        account.signTransaction(transaction)
+        .then((signedTransaction)=>{
+            web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
+            .on('error', function(error){ console.error(account.address,error) })
+            .then(function(receipt){
+                console.log("Claim made",receipt)
+                resolve()
+            });
+        })
     })
 }
 
@@ -112,19 +149,19 @@ getSignature=(account, claimType, data)=>
     return signatureObject
 }
 
-createClaim=(account, claimData)=>
+createClaim=(account, claimContent)=>
 {
     let scheme = 1
     let claimType = 1
-
-    let signatureObject = getSignature(account, claimType, claimData)
+    let claimData = web3.utils.keccak256(claimContent)
+    let signature = getSignature(account, claimType, claimData).signature
 
     let claim = {
         scheme:scheme,
         claimType : claimType,
-        data : claimData, //signatureObject.messageHash?
-        uri : "",
-        signature : signatureObject.signature
+        data : claimData,
+        uri : "Location of the claim",//Voting contract address?
+        signature : signature
     }
     return claim
 }
@@ -132,10 +169,9 @@ createClaim=(account, claimData)=>
 makeClaim=(issuerAccount, recieverAddress, claim)=>
 {
     return new Promise((resolve, reject)=>{
-
-        console.log(dids[recieverAddress])
         
-        let claimTransaction = dids[recieverAddress].methods.addClaim(claim.claimType, claim.scheme, claim.issuer, claim.signature, claim.data, claim.uri)
+        let identityContract = createIdentityContractInstance(recieverAddress)
+        let claimTransaction = identityContract.methods.addClaim(claim.claimType, claim.scheme, claim.issuer, claim.signature, claim.data, claim.uri)
         let encodedAbi = claimTransaction.encodeABI()
         let transaction = {
             gas: deployIdentityGasCost,
@@ -147,7 +183,7 @@ makeClaim=(issuerAccount, recieverAddress, claim)=>
         issuerAccount.signTransaction(transaction)
         .then((signedTransaction)=>{
             web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
-            .on('error', function(error){ console.error(account.address,error) })
+            .on('error', function(error){ console.error(issuerAccount.address,error) })
             //.on('transactionHash', function(transactionHash){ console.log("transaction hash", account.address,transactionHash)})
             //.on('confirmation', function(confirmationNumber, receipt){ console.log ("confirmationNumber",account.address,confirmationNumber) })
             //.on('receipt', function(receipt){ console.log("receipt", account.address,receipt.blockNumber)})
@@ -164,6 +200,7 @@ run =()=>{
         //web3.eth.getAccounts() // We don't use getAccounts() because sign function is not exposed
         accounts =  createAllAccounts(10)
         createAllDids(accounts)
+        .then(()=>addKey(accounts[EMITTER], dids[accounts[EMITTER]], accounts[EMITTER], 3, 1 ))
         .then(()=>createClaim(accounts[EMITTER], CLAIM_CONTENT))
         .then((claim)=>makeClaim(accounts[EMITTER], accounts[USER1].address, claim))
         .then(resolve)
